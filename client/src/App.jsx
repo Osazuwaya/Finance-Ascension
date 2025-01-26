65cde0c8614cbe2751204cab2ae35f7382a0f4d4
ProudOne7 <peternyen7@gmail.com>
2025-01-26 11:46:18 -0600

    added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,16 @@ import Stats from './components/stats/stats.jsx';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 import MainPage from './components/MainPage/MainPage'
+import LoginPage from './components/login/login.jsx'
+import StatPage from './components/stats/stats.jsx'
 
 function App() {
   return (
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<MainPage/>}></Route>
+          <Route path='/' element={<LoginPage/>}></Route>
           <Route path='/main-page' element={<MainPage/>}></Route>
+          <Route path='/stats-page' element={<StatPage/>}></Route>
         </Routes>
       </BrowserRouter>
   )
