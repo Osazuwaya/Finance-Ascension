83da4fc769513b0b898a35283caad73a98b0aa4c
ProudOne7 <peternyen7@gmail.com>
2025-01-26 11:47:28 -0600

    added char imgs

--- client/src/components/MainPage/MainPage.jsx
+++ client/src/components/MainPage/MainPage.jsx
@@ -3,8 +3,15 @@ import { RiArrowRightLine } from "react-icons/ri"
 import React, { useState } from 'react'
 import { motion } from "framer-motion";
 
+import Char1 from '../../../imgs/char1.png'
+import Char2 from '../../../imgs/char2.png'
+import Char3 from '../../../imgs/char3.png'
+import Char4 from '../../../imgs/char4.gif'
+import Char5 from '../../../imgs/char5.gif'
+import { useNavigate } from 'react-router-dom';
 
 function MainPage() {
+    const navigate = useNavigate()
     const [charSelect, setCharSelect] = useState(false);
 
   return (
@@ -16,36 +23,36 @@ function MainPage() {
             <Flex w={'100%'} h={'40%'} flexDir={'row'} flexBasis={'65%'} justify={'space-evenly'} align={'center'} bg={'#6D597A'}>
                 <Card.Root w={{xl: '12.5rem'}} h={'85%'} >
                     <Card.Body flexDir={'row'} gap='2rem'>
-                        <Image></Image>
+                        <Image src={Char1}></Image>
                     </Card.Body>
                 </Card.Root>
 
                 <Card.Root w={{xl: '12.5rem'}} h={'85%'}>
                     <Card.Body flexDir={'row'} gap='2rem'>
-                        <Image></Image>
+                        <Image src={Char2}></Image>
                     </Card.Body>
                 </Card.Root>
 
                 <Card.Root w={{xl: '12.5rem'}} h={'85%'}>
                     <Card.Body flexDir={'row'} gap='2rem'>
-                        <Image></Image>
+                        <Image src={Char3}></Image>
                     </Card.Body>
                 </Card.Root>
 
                 <Card.Root w={{xl: '12.5rem'}} h={'85%'}>
                     <Card.Body flexDir={'row'} gap='2rem'>
-                        <Image></Image>
+                        <Image src={Char4}></Image>
                     </Card.Body>
                 </Card.Root>
 
                 <Card.Root w={{xl: '12.5rem'}} h={'85%'}>
                     <Card.Body flexDir={'row'} gap='2rem'>
-                        <Image></Image>
+                        <Image src={Char5}></Image>
                     </Card.Body>
                 </Card.Root>                
             </Flex>
             <Flex w={'100%'} h={'40%'} flexBasis={'20%'} bg={'#6D597A'} justify={'center'} align={'center'}>
-                <Button size={'lg'} color={'white'} bg={'#B56576'} borderWidth={'.15rem'} animation={'scale-out'}>
+                <Button size={'lg'} color={'white'} bg={'#B56576'} borderWidth={'.15rem'} animation={'scale-out'} onClick={() => navigate('/stats-page')}>
                     Continue <RiArrowRightLine />
                 </Button>
             </Flex>
